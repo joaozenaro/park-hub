de382b54e027d8a7c2d0ab76e24f987bcb10945f
zjefersound <jeferson.2017317264@gmail.com>
2024-06-07 01:00:00 +0000

    Routes setup done

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,31 +1,7 @@
-import { Routes, Route, Link } from 'react-router-dom';
-import Home from './pages/Home';
-import About from './pages/About';
-import NotFound from './pages/NotFound';
-import Confirm from './pages/Confirm';
+import Routes from "./routes";
 
 export default function App() {
   return (
-    <div>
-      <nav>
-        <ul>
-          <li>
-            <Link to="/">Home</Link>
-          </li>
-          <li>
-            <Link to="/about">About</Link>
-          </li>
-          <li>
-            <Link to="/nskajdns">Non existant</Link>
-          </li>
-        </ul>
-      </nav>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/about" element={<About />} />
-        <Route path="/confirm" element={<Confirm />} />
-        <Route path="*" element={<NotFound />} />
-      </Routes>
-    </div>
+    <Routes />
   );
 }
