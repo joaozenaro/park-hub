bf3d6f3e11fd4b7682836839ce0da608605b5ea5
zjefersound <jeferson.2017317264@gmail.com>
2024-06-19 23:27:31 +0000

    set font + button styles

--- frontend/src/components/ui/Button.tsx
+++ frontend/src/components/ui/Button.tsx
@@ -13,13 +13,13 @@ interface Props {
 export function Button({ type = 'primary', children, onClick, className, ...props }: Props) {
   return (
     <button
-      className={clsx("border text-white rounded flex justify-center items-center p-2 font-bold transition disabled:opacity-75", {
-        "bg-amber-500 hover:bg-amber-600 active:bg-amber-700": type === "brand",
-        "bg-neutral-900 hover:bg-neutral-800 active:bg-neutral-700": type === "primary",
-        "bg-neutral-600 hover:bg-neutral-500 active:bg-neutral-400": type === "secondary",
+      className={clsx("rounded-md flex justify-center items-center py-2.5 px-3.5 text-sm leading-4 font-semibold transition disabled:opacity-75 space-x-2.5", {
+        "bg-amber-500 hover:bg-amber-600 active:bg-amber-700 text-white": type === "brand",
+        "bg-neutral-900 hover:bg-neutral-800 active:bg-neutral-700 text-white": type === "primary",
+        "bg-slate-500 hover:bg-slate-600 active:bg-slate-700 text-white": type === "secondary",
         "bg-slate-200 hover:bg-slate-300 active:bg-slate-400 text-neutral-900": type === "tertiary",
-        "bg-emerald-600 hover:bg-emerald-400 active:bg-emerald-300": type === "success",
-        "bg-red-600 hover:bg-red-400 active:bg-red-300": type === "danger",
+        "bg-emerald-600 hover:bg-emerald-700 active:bg-emerald-800 text-white": type === "success",
+        "bg-red-600 hover:bg-red-700 active:bg-red-800 text-white": type === "danger",
       })}
       onClick={onClick}
       {...props}
