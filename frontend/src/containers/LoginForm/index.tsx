06583b92d7ceec288774046405fc4fb45f56440a
Jeferson Santos Moraes de Souza <62676057+zjefersound@users.noreply.github.com>
2024-06-21 01:31:16 +0000

    Create form components

--- frontend/src/containers/LoginForm/index.tsx
+++ frontend/src/containers/LoginForm/index.tsx
@@ -1,5 +1,5 @@
 import { useNavigate } from "react-router-dom";
-import { FieldError } from "../../components/form/FieldError";
+import FieldError from "../../components/form/FieldError";
 import { useForm } from "../../hooks/useForm";
 import { ILoginForm } from "../../models/ILoginForm";
 import { isValidLogin } from "./validation";
@@ -8,20 +8,24 @@ import { authService } from "../../services/authService";
 import axios, { AxiosError } from "axios";
 import { IApiErrorResponse } from "../../models/IApiResponse";
 import { Button } from "../../components/ui/Button";
+import { FormControl } from "../../components/form/FormControl";
+import { TextInput } from "../../components/form/TextInput";
+import { Loading } from "../../components/ui/Loading";
 
 const defaultData = {
   username: "",
-  password: ""
-}
+  password: "",
+};
 export default function LoginForm() {
   const navigate = useNavigate();
   const { setToken } = useAuth();
   const onSubmit = async (data: ILoginForm) => {
-    await authService.login(data)
-      .then(res => {
+    await authService
+      .login(data)
+      .then((res) => {
         if (res.data.success) {
-          setToken(res.data.data.access_token)
-          navigate('/');
+          setToken(res.data.data.access_token);
+          navigate("/");
         }
       })
       .catch((err: AxiosError<IApiErrorResponse>) => {
@@ -31,8 +35,8 @@ export default function LoginForm() {
         } else {
           console.error(err);
         }
-      })
-  }
+      });
+  };
 
   const { data, loading, errors, handleChangeValue, handleSubmit } =
     useForm<ILoginForm>({
@@ -42,20 +46,41 @@ export default function LoginForm() {
     });
   return (
     <form onSubmit={handleSubmit}>
-      {loading && <p>Loading...</p>}
-      <div>
-        <label>Username</label>
-        <input id="username" className="border p-3" type="text" value={data.username} onChange={e => handleChangeValue("username", e.target.value)} />
-        <FieldError id="username" errors={errors} />
-      </div>
-      <div>
-        <label>Password</label>
-        <input id="password" className="border p-3" type="password" value={data.password} onChange={e => handleChangeValue("password", e.target.value)} />
-        <FieldError id="password" errors={errors} />
+      <div className="p-8">
+        <FormControl id="username" label="Username" errors={errors}>
+          <TextInput.Root>
+            <TextInput.Icon>
+              {/* <AiOutlineUser /> */}
+              <p>:0</p>
+            </TextInput.Icon>
+            <TextInput.Input
+              value={data.username}
+              onChange={(e) => handleChangeValue("username", e.target.value)}
+              placeholder="Digite o username..."
+              required
+            />
+          </TextInput.Root>
+        </FormControl>
+        <FormControl id="password" label="Senha" errors={errors}>
+          <TextInput.Root>
+            <TextInput.Icon>
+              {/* <AiOutlineLock /> */}
+              <p>:D</p>
+            </TextInput.Icon>
+            <TextInput.Input
+              type="password"
+              value={data.password}
+              onChange={(e) => handleChangeValue("password", e.target.value)}
+              placeholder="******"
+              required
+            />
+          </TextInput.Root>
+        </FormControl>
       </div>
       <Button>
+        {loading && <Loading size="sm" />}
         Entrar
       </Button>
     </form>
-  )
+  );
 }
