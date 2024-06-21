445b532088b4f881f9b8c88cb18bb602cd174e63
Jeferson Santos Moraes de Souza <62676057+zjefersound@users.noreply.github.com>
2024-06-21 02:27:02 +0000

    Create Header

---
+++ frontend/src/components/layout/Header.tsx
@@ -0,0 +1,11 @@
+export default function Header() {
+  return (
+    <div className="h-16 shrink-0 border-b border-slate-300 bg-white sticky top-0 z-10 flex items-center px-8">
+      <div className="ml-auto">
+        <button>
+          <p>Caboclo</p>
+        </button>
+      </div>
+    </div>
+  );
+}
--- frontend/src/components/layout/SidebarPageLayout.tsx
+++ frontend/src/components/layout/SidebarPageLayout.tsx
@@ -1,11 +1,13 @@
 import { PropsWithChildren } from "react";
 import SideBar from "./SideBar";
+import Header from "./Header";
 
 export default function SidebarPageLayout({ children }: PropsWithChildren) {
   return (
     <div className="flex flex-1">
       <SideBar />
       <div className="flex flex-1 flex-col overflow-auto">
+        <Header />
         {children}
       </div>
     </div>
