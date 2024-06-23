2ae6a493cd3a4b3b97bd90be9ec05f05ef3f8c77
Jeferson Santos Moraes de Souza <62676057+zjefersound@users.noreply.github.com>
2024-06-23 00:08:03 +0000

    pages placeholder

---
+++ frontend/src/pages/CompleteSignup.tsx
@@ -0,0 +1,10 @@
+export default function CompleteSignup() {
+  // TO DO: Get id and token from query string, collect user name, username and password with a confirmation input and send a request to /complete-signup
+
+  return (
+    <div className="grid place-items-center h-full w-full">
+      <h1>Work in progress</h1>
+      <img src="https://media1.tenor.com/m/J1kq1w0eB2AAAAAC/sasiad-o-6.gif" />
+    </div>
+  )
+}
