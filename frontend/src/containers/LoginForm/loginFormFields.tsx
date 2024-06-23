a0945ad5efbb52679dd83d859d39258af574f821
Jeferson Santos Moraes de Souza <62676057+zjefersound@users.noreply.github.com>
2024-06-23 13:08:58 +0000

    Login with smart form fields

---
+++ frontend/src/containers/LoginForm/loginFormFields.tsx
@@ -0,0 +1,20 @@
+import { MdOutlineEmail } from "react-icons/md";
+import { IFieldProps } from "../../components/form/SmartField";
+import { AiOutlineLock } from "react-icons/ai";
+
+export const loginFormFields: IFieldProps[] = [
+  {
+    Icon: MdOutlineEmail,
+    id: "username",
+    label: "Email",
+    placeholder: "Digite o seu email",
+    type: "text",
+  },
+  {
+    Icon: AiOutlineLock,
+    id: "password",
+    label: "Senha",
+    placeholder: "*******",
+    type: "password",
+  },
+];
