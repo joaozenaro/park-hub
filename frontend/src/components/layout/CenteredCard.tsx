4704a371af2167f59ded37150273ee07629d8a33
Jeferson Santos Moraes de Souza <62676057+zjefersound@users.noreply.github.com>
2024-06-23 02:44:43 +0000

    fix ui bugs

--- frontend/src/components/layout/CenteredCard.tsx
+++ frontend/src/components/layout/CenteredCard.tsx
@@ -8,7 +8,7 @@ interface Props {
 export default function CenteredCard({ children, className }: Props) {
   return (
     <div className="flex flex-1 bg-zinc-900">
-      <div className={clsx("max-w-lg m-auto flex flex-col bg-white p-8 rounded-xl  shadow-slate-300 w-[500px] h-[450px] overflow-auto scrollbar", className)}>
+      <div className={clsx("max-w-lg m-auto flex flex-col bg-white p-8 rounded-xl  shadow-slate-300 w-[500px] h-[480px] overflow-auto scrollbar", className)}>
         {children}
       </div>
     </div >
