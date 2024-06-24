f08866789e580d8638e6ff50e64984d0dcc1ca7c
João Zenaro <joaoz@email.com>
2024-06-24 16:16:53 +0000

    feat: Backend user search update delete

---
+++ backend/core/models/SearchModel.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace app\core\models;
+
+use Yii;
+use yii\base\Model;
+
+class SearchModel extends Model
+{
+    public $searchTerm;
+    public $skip;
+    public $take;
+    public $startDate;
+    public $endDate;
+
+    public function rules()
+    {
+        return [
+            ['searchTerm', 'string'],
+            [['skip', 'take'], 'integer'],
+            [['startDate', 'endDate'], 'datetime', 'format' => 'php:Y-m-d H:i:s'],
+        ];
+    }
+
+    public function attributeLabels()
+    {
+        return [
+            'searchTerm' => Yii::t('app', 'Termo de busca'),
+            'startDate' => Yii::t('app', 'Data inicial'),
+            'endDate' => Yii::t('app', 'Data final'),
+        ];
+    }
+}
