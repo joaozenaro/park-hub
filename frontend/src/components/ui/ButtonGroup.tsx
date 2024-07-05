842c7da55d81dc5146607bf4aeea33f7f9a22b2d
Jeferson Santos Moraes de Souza <62676057+zjefersound@users.noreply.github.com>
2024-07-05 22:53:03 +0000

    update button group

--- frontend/src/components/ui/ButtonGroup.tsx
+++ frontend/src/components/ui/ButtonGroup.tsx
@@ -1,6 +1,6 @@
 import { Slot } from "@radix-ui/react-slot";
 import clsx from "clsx";
-import { ReactNode } from "react";
+import React, { ReactNode, Ref } from "react";
 
 interface ButtonGroupRootProps {
   className?: string;
@@ -8,19 +8,20 @@ interface ButtonGroupRootProps {
   asChild?: boolean;
 }
 
-export function ButtonGroupRoot({
-  children,
-  className,
-  asChild,
-}: ButtonGroupRootProps) {
-  const Comp = asChild ? Slot : "div";
+const ButtonGroupRoot = React.forwardRef(
+  ({ children, className, asChild }: ButtonGroupRootProps, forwardedRef) => {
+    const Comp = asChild ? Slot : "div";
 
-  return (
-    <Comp className={clsx("inline-flex -space-x-px text-sm", className)}>
-      {children}
-    </Comp>
-  );
-}
+    return (
+      <Comp
+        ref={forwardedRef as Ref<HTMLDivElement>}
+        className={clsx("inline-flex -space-x-px text-sm", className)}
+      >
+        {children}
+      </Comp>
+    );
+  }
+);
 
 interface ButtonGroupButtonProps {
   className?: string;
@@ -30,34 +31,40 @@ interface ButtonGroupButtonProps {
   disabled?: boolean;
   active?: boolean;
 }
-export function ButtonGroupButton({
-  children,
-  className,
-  asChild,
-  active,
-  disabled,
-  onClick,
-}: ButtonGroupButtonProps) {
-  const Comp = asChild ? Slot : "button";
+const ButtonGroupButton = React.forwardRef(
+  (
+    {
+      children,
+      className,
+      asChild,
+      active,
+      disabled,
+      onClick,
+    }: ButtonGroupButtonProps,
+    forwardedRef
+  ) => {
+    const Comp = asChild ? Slot : "button";
 
-  return (
-    <Comp
-      onClick={onClick}
-      disabled={disabled}
-      className={clsx(
-        "flex items-center justify-center px-3 h-8 leading-tight border border-slate-300 disabled:pointer-events-none",
-        "first:rounded-l-md last:rounded-r-md",
-        className,
-        {
-          "bg-zinc-900 text-white hover:bg-zinc-800": active,
-          "text-slate-500 bg-white hover:bg-slate-100": !active,
-        }
-      )}
-    >
-      {children}
-    </Comp>
-  );
-}
+    return (
+      <Comp
+        ref={forwardedRef as Ref<HTMLButtonElement>}
+        onClick={onClick}
+        disabled={disabled}
+        className={clsx(
+          "flex items-center justify-center px-3 h-8 leading-tight border border-slate-300 disabled:pointer-events-none",
+          "first:rounded-l-md last:rounded-r-md",
+          className,
+          {
+            "bg-zinc-900 text-white hover:bg-zinc-800": active,
+            "text-slate-500 bg-white hover:bg-slate-100": !active,
+          }
+        )}
+      >
+        {children}
+      </Comp>
+    );
+  }
+);
 
 export const ButtonGroup = {
   Root: ButtonGroupRoot,
--- frontend/src/components/ui/ToggleGroup.tsx
+++ frontend/src/components/ui/ToggleGroup.tsx
@@ -1,5 +1,6 @@
 import * as ToggleGroupPrimitive from "@radix-ui/react-toggle-group";
 import { ISelectOption } from "../../models/ISelectOption";
+import { ButtonGroup } from "./ButtonGroup";
 
 interface Props {
   value: string;
@@ -15,23 +16,25 @@ export const ToggleGroup = ({
   ...props
 }: Props) => (
   <ToggleGroupPrimitive.Root
-    className="inline-flex bg-slate-200 rounded-md border space-x-px"
     type="single"
     defaultValue={value}
     aria-label={props["aria-label"]}
     onValueChange={onChangeValue}
+    asChild
   >
-    {options.map((option) => (
-      <ToggleGroupPrimitive.Item
-        key={option.value}
-        className={
-          "hover:bg-slate-100 text-slate-500 data-[state=on]:bg-zinc-900 data-[state=on]:text-white flex h-[36px] w-[36px] items-center justify-center bg-white text-base leading-4 first:rounded-l-md last:rounded-r-md focus:z-10 focus:shadow-slate-100 focus:outline-none"
-        }
-        value={option.value}
-        aria-label={option.value}
-      >
-        {option.label}
-      </ToggleGroupPrimitive.Item>
-    ))}
+    <ButtonGroup.Root>
+      {options.map((option) => (
+        <ToggleGroupPrimitive.Item
+          key={option.value}
+          value={option.value}
+          aria-label={option.value}
+          asChild
+        >
+          <ButtonGroup.Button active={option.value === value}>
+            {option.label}
+          </ButtonGroup.Button>
+        </ToggleGroupPrimitive.Item>
+      ))}
+    </ButtonGroup.Root>
   </ToggleGroupPrimitive.Root>
 );
